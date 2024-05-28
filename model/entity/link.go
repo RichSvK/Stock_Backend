ba3648358c41681f620181149530c535e6716c9b
Richard Sugiharto <richardsugiharto0@gmail.com>
2024-05-29 03:38:52 +0700

    Fix Link primary key

--- model/entity/link.go
+++ model/entity/link.go
@@ -2,7 +2,7 @@ package entity
 
 type Link struct {
 	Name        string `json:"web_name" gorm:"type:VARCHAR(50);column:web_name;not null"`
-	URL         string `json:"web_url_link" gorm:"type:TEXT;;column:url_link;not null"`
+	URL         string `json:"web_url_link" gorm:"type:TEXT;column:url_link;primaryKey"`
 	Image       string `json:"web_image" gorm:"type:TEXT;column:web_image;not null"`
 	Description string `json:"web_description" gorm:"type:TEXT;column:web_description;not null"`
 	CategoryID  int    `json:"category_id" gorm:"type:INT;not null"`
